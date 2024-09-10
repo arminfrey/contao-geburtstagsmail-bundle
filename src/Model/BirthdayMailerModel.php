bc765ef134e66f0004cc43c3272e05558e9c79af
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2024-09-10 15:28:50 +0000

    Create BirthdayMailerModel.php

---
+++ src/Model/BirthdayMailerModel.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of BirthdayMailer.
+ * 
+ * (c) Armin Frey 2022 <webmaster@krettenweiber.de>
+ * @license GPL-3.0-or-later
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ * @link https://github.com/arminfrey/BirthdayMailer
+ */
+
+namespace Arminfrey\BirthdayMailer\Model;
+
+use Contao\Model;
+
+/**
+ * Class BirthdayMailerModel
+ *
+ * @package Arminfrey\BirthdayMailer\Model
+ */
+class JungbuettelModel extends Model
+{
+    protected static $strTable = 'tl_birthdaymailer';
+
+}
