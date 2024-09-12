5de6713332d2cc825c4d2e9bd97a83d86abb4f96
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2024-09-12 14:57:22 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,5 +1,6 @@
 <?php
 
+use Arminfrey\GeburtstagsmailBundle\Model\GeburtstagsmailModel;
 /**
  * -------------------------------------------------------------------------
  * BACK END MODULES
@@ -10,17 +11,18 @@
 $GLOBALS['BE_MOD']['accounts']['Geburtstagsmail'] = array
 (
 	'tables'           => array('tl_geburtstagsmail'),
-	'icon'             => '../assets/icon.png',
+	'icon'             => '/assets/icon.png',
 	'sendBirthdayMail' => array('GeburtstagsmailBundle', 'sendBirthdayMailManually'), 
 );
+
+
+$GLOBALS['TL_MODELS']['tl_geburtstagsmail'] = GeburtstagsmailModel::class;
 
 /**
  * -------------------------------------------------------------------------
  * CRON
  * -------------------------------------------------------------------------
  */
-
 // Daily cron job to send birthday mails
 $GLOBALS['TL_CRON']['daily'][] = array('GeburtstagsmailBundle', 'sendBirthdayMail');
 
-?>
