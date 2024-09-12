125cb56e1d50cf2668647748b304e18a5a3f6168
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2024-09-12 12:27:47 +0200

    Update and rename modules.php to modules.php

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Define name and tooltip for preferences (inactive modules)
+ */
+$GLOBALS['TL_LANG']['MOD']['BirthdayMailer'] = array('Birthdaymailer', 'Sends a birthday email to all the members having their birtday on the current day.');
+
+?>
