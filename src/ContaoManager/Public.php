4204d986619f018e581bf113bf07268705f5fe64
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2024-09-12 10:25:08 +0200

    Update Public.php

--- src/ContaoManager/Public.php
+++ src/ContaoManager/Public.php
@@ -3,20 +3,22 @@
 declare(strict_types=1);
 
 /*
- * This file is part of BirthdayMailer.
+ * This file is part of GeburtstagsmailBundle.
  * 
- * (c) Armin Frey 2022 <webmaster@krettenweiber.de>
+ * (c) Armin Frey 2024 <webmaster@krettenweiber.de>
  * @license GPL-3.0-or-later
  * For the full copyright and license information,
  * please view the LICENSE file that was distributed with this source code.
  * @link https://github.com/freyar/contao-jugend-bundle
  */
 
-namespace Arminfrey\BirthdayMailer\ContaoManager;
+namespace Arminfrey\GeburtstagsmailBundle\ContaoManager;
 
+use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Arminfrey\GeburtstagsmailBundle\GeburtstagsmailBundle;
 
 /**
  * Class Plugin
@@ -26,8 +28,8 @@ class Plugin implements BundlePluginInterface
     public function getBundles(ParserInterface $parser): array
     {
         return [
-            BundleConfig::create('Arminfrey\BirthdayMailer\ArminfreyBirthdayMailer')
-                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle']),
+            BundleConfig::create('GeburtstagsmailBundle')
+                ->setLoadAfter(['ContaoCoreBundle']),
         ];
     }
 }
