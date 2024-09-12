ed6641005e9bd2665858b5bf4a417390892b7d10
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2024-09-12 12:24:22 +0200

    Update and rename tl_birthdaymailer.php to tl_geburtstagsmail.php

---
+++ src/Resources/contao/languages/de/tl_geburtstagsmail.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2015 Leo Feyer
+ 
+ * @author     Cliff Parnitzky
+ * @package    GeburtstagsmailBundle
+ * @license    LGPL
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['memberGroup']       = array('Mitgliedergruppe', 'Bitte wählen Sie die Mitgliedergruppe aus, die automatische Geburtstagsemails erhalten soll.');
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['priority']          = array('Priorität', 'Bitte geben Sie einen Prioritätswert für diese Konfiguration ein. Bei mehreren Konfigurationen, die für ein Mitglied zutreffen, wird immer die mit dem höchsten Wert zuerst verwendet.');
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['sender']            = array('Absenderadresse', 'Bitte geben Sie die E-Mail-Adresse für den Absender ein.');
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['senderName']        = array('Absendername', 'Bitte geben Sie einen individuellen Namen für den Absender ein.');
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['mailCopy']          = array('Kopie an (CC)', 'Bitte geben Sie eine Liste kommagetrennter E-Mail-Adressen an, die eine Kopie der Geburtstagsemail erhalten sollen.');
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['mailBlindCopy']     = array('Blindkopie an (BCC)', 'Bitte geben Sie eine Liste kommagetrennter E-Mail-Adressen an, die eine Blindkopie der Geburtstagsemail erhalten sollen.');
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['mailUseCustomText'] = array('Eigene E-Mail Texte verwenden', 'Bitte geben Sie ob statt der Standardinhalte (<i>Anrede, Betreff, HTML, Text</i>) in der E-Mail eigene Texte verwendet werden sollen.');
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['mailTextKey']       = array('Schlüssel für eigene E-Mail Texte', 'Bitte geben Sie den Schlüssel für die eigenen E-Mail Texte. Dieser wird benötigt um die eigenen Texte zu ermitteln.<br/><br/>Beispiel (Eintrag in <i>system/config/langconfig.php</i>):<br/><code>$GLOBALS[\'TL_LANG\'][\'BirthdayMailer\'][\'mail\'][\'<b>MEIN_SCHLUESSEL</b>\'][\'subject\'][\'en\'] = \'English Custom Subject\'; </code>');
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['config_legend'] = 'Konfiguration';
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['email_legend']  = 'Emaileinstellungen';
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['sendBirthdayMail'] = array('Manuelle Ausführung', 'Senden der Geburtstagsmails manuell ausführen');
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['new']              = array('Neue Konfiguration', 'Eine neue Konfiguration für Geburtstagsmails anlegen');
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['show']             = array('Konfigurationsdetails', 'Details der Konfiguration ID %s anzeigen');
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['edit']             = array('Konfiguration bearbeiten', 'Konfiguration ID %s bearbeiten');
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['copy']             = array('Konfiguration duplizieren', 'Konfiguration ID %s duplizieren');
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['delete']           = array('Konfiguration löschen', 'Konfiguration ID %s löschen');
+
+/**
+ * Manual execution messages
+ */
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['manualExecution']['headline']          = "Manuelle Ausführung";
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['manualExecution']['sendingHeadline']   = "Systemnachrichten";
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['manualExecution']['successMessage']    = "%s E-Mails wurden erfolgreich versendet.";
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['manualExecution']['failureMessage']    = "%s E-Mails konnten wegen Fehler nicht gesendet werden.";
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['manualExecution']['failureTableHead']  = "Fehler";
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['manualExecution']['failureInfo']       = "Bitte prüfen Sie das Contao <b>System-Log</b> oder die Log-Dateien (<i>birthdaymails.log</i>, <i>error.log</i>) um weitere Informationen zu den Fehlern zu erhalten.";
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['manualExecution']['abortionMessage']   = "%s E-Mails konnten wegen Abbrüchen (durch Hooks) nicht gesendet werden.";
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['manualExecution']['abortionTableHead'] = "Abbrüche";
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['manualExecution']['abortionInfo']      = "Bitte prüfen Sie das Contao <b>System-Log</b> um weitere Informationen zu den Abbrüchen zu erhalten.";
+$GLOBALS['TL_LANG']['tl_geburtstagsmail']['manualExecution']['developerMessage']  = "Sie befinden sich im Entwicklermodus. Alle E-Mails werden an die folgende Entwickler E-Mail-Adresse gesendet: <i>%s</i>. Bitte stellen Sie sicher, dass dies eine gültige E-Mail-Adresse ist. Änderungen können in den <b>Einstellungen</b> vorgenommen werden.";
+
+?>
