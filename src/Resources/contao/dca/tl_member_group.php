a8273f421949ed2f26c3f4b6427529c75781e6fe
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2024-09-13 08:54:29 +0200

    Update tl_member_group.php

--- src/Resources/contao/dca/tl_member_group.php
+++ src/Resources/contao/dca/tl_member_group.php
@@ -4,7 +4,7 @@
  * Contao Open Source CMS
  * Copyright (C) 2005-2015 Leo Feyer
  *
- * @author     Cliff Parnitzky
+ * @author     Armin Frey
  * @package    GeburtstagsMailBundle
  * @license    LGPL
  */
@@ -12,6 +12,6 @@
 /**
  * Delete an according BirthdayMailer configuration, if the member group is deleted.
  */
-$GLOBALS['TL_DCA']['tl_member_group']['config']['ondelete_callback'][] = array('GeburtstagsmailBundle', 'deleteConfiguration');
+$GLOBALS['TL_DCA']['tl_member_group']['config']['ondelete_callback'][] = array('ArminfreyGeburtstagsmailBundle', 'deleteConfiguration');
 
 ?>
