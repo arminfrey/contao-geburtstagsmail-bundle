34aaaaaf596d108b65d3ea8b2f7b7691ae98a607
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2024-09-20 13:32:10 +0200

    Update ArminfreyGeburtstagsmailBundle.php

--- src/ArminfreyGeburtstagsmailBundle.php
+++ src/ArminfreyGeburtstagsmailBundle.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 
-class GeburtstagsmailBundle extends Bundle
+class ArminfreyGeburtstagsmailBundle extends Bundle
 {
 	const DEFAULT_LANGUAGE = 'de';
 	
