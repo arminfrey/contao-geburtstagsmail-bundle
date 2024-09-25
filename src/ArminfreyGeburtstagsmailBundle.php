4244df30fbe34b5214fcd552245d654a416b3c66
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2024-09-25 13:35:27 +0200

    Update ArminfreyGeburtstagsmailBundle.php

--- src/ArminfreyGeburtstagsmailBundle.php
+++ src/ArminfreyGeburtstagsmailBundle.php
@@ -7,10 +7,10 @@ use Contao\System;
 use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Arminfrey\GeburtstagsmailBundle\DependencyInjection\ArminfreyGeburtstagsmailExtension;
+//use Arminfrey\GeburtstagsmailBundle\DependencyInjection\ArminfreyGeburtstagsmailExtension;
 /*use Arminfrey\GeburtstagsmailBundle\Model\ArminfreyGeburtstagsmailModel;*/
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+//use Symfony\Component\HttpFoundation\Request;
+//use Symfony\Component\HttpFoundation\Response;
 
 
 class ArminfreyGeburtstagsmailBundle extends Bundle
