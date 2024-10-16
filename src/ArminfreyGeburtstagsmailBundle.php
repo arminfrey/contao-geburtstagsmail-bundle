c3a14aba27a7b64835632cfd1bb8a955ed055f2e
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2024-10-16 15:08:07 +0200

    Update ArminfreyGeburtstagsmailBundle.php

--- src/ArminfreyGeburtstagsmailBundle.php
+++ src/ArminfreyGeburtstagsmailBundle.php
@@ -11,8 +11,8 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Arminfrey\GeburtstagsmailBundle\DependencyInjection\ArminfreyGeburtstagsmailExtension;
 /*use Arminfrey\GeburtstagsmailBundle\Model\ArminfreyGeburtstagsmailModel;*/
-//use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class ArminfreyGeburtstagsmailBundle extends Bundle
