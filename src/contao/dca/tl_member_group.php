48527638a00083733047917a5d823ace1f2a3617
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2024-10-31 11:22:08 +0100

    Update tl_member_group.php

--- src/contao/dca/tl_member_group.php
+++ src/contao/dca/tl_member_group.php
@@ -12,6 +12,6 @@
 /**
  * Delete an according BirthdayMailer configuration, if the member group is deleted.
  */
-$GLOBALS['TL_DCA']['tl_member_group']['config']['ondelete_callback'][] = array('ArminfreyGeburtstagsmailBundle', 'deleteConfiguration');
+$GLOBALS['TL_DCA']['tl_member_group']['config']['ondelete_callback'][] = ['ArminfreyGeburtstagsmailBundle', 'deleteConfiguration'];
 
 ?>
