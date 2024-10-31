6041f0ffe1a10b16d9902b5d19d7cbcfce0a5941
Armin Frey <104427130+arminfrey@users.noreply.github.com>
2024-10-31 13:36:49 +0100

    Update ArminfreyGeburtstagsmailExtension.php

--- src/DependencyInjection/ArminfreyGeburtstagsmailExtension.php
+++ src/DependencyInjection/ArminfreyGeburtstagsmailExtension.php
@@ -31,8 +31,8 @@ class ArminfreyGeburtstagsmailExtension extends Extension
      */
    public function load(array $configs, ContainerBuilder $container): void
     {
-        //$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config/'));
-        $loader = new YamlFileLoader($container, new FileLocator('/../config/'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../config/'));
+        //$loader = new YamlFileLoader($container, new FileLocator('/../config/'));
         $loader->load('services.yaml');
     }
 }
